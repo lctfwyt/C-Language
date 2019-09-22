18cd60d16e52ebb83577adb526ae526fb43c3fd9
乙淅君 <lctfwyt@outlook.com>
2019-09-22 20:51:16 +1000

    complete ex1

---
+++ ex1.c
@@ -0,0 +1,13 @@
+// #include <stdio.h>
+
+int main(int argc, char *argv[])
+// int main()
+// int main(void)
+
+{
+	puts("Hello world.");
+	puts("Meow");
+	puts("Puts. Puts. Puts.");
+	
+	return 0; // can be omitted.
+}
