1347fed50af1550b993d62ee12608c16f1eae63f
乙淅君 <lctfwyt@outlook.com>
2019-09-23 19:22:08 +1000

    complete ex4

---
+++ ex4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/* Warning: This program is wrong for purpose. */
+/* simply use "sudo apt-get install valgrind" */
+
+int main()
+{
+	int age = 10;
+	int height;
+	// int height = 72;
+	
+	printf("I am %d years old.\n");
+	// printf("I am %d years old.\n", age);
+	printf("I am %d inches tall.\n", height);
+	
+	return 0;
+}
